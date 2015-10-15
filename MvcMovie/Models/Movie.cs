60780de038b7027e53a17319a9ae6c6b2c6cfea9
John Albright <jalbright10@gmail.com>
2015-10-15 12:55:14 -0400

    Converted spaces into tabs

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -4,17 +4,17 @@ using System.Data.Entity;
 
 namespace MvcMovie.Models
 {
-    public class Movie
-    {
-        public int Id { get; set; }
+	public class Movie
+	{
+		public int Id { get; set; }
 
 		[StringLength(60, MinimumLength = 3)]
 		public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
-        public DateTime ReleaseDate { get; set; }
+		[Display(Name = "Release Date")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+		public DateTime ReleaseDate { get; set; }
 
 		[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
 		[Required]
@@ -28,10 +28,10 @@ namespace MvcMovie.Models
 		[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
 		[StringLength(5)]
 		public string Rating { get; set; }
-    }
+	}
 
-    public class MovieDBContext : DbContext
-    {
-        public DbSet<Movie> Movies { get; set; }
-    }
+	public class MovieDBContext : DbContext
+	{
+		public DbSet<Movie> Movies { get; set; }
+	}
 }
